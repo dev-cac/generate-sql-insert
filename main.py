e8bfc317fc30bf7c91f58acaf2b90351f47ac224
Luis Osorio <luisfosgdev@gmail.com>
2023-05-08 16:31:24 -0500

    feat: agregando for para ver información obtenida y testear

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pandas as pd
 
-df = pd.read_excel('./input.xlsx', sheet_name=1)
+df = pd.read_excel('./input.xlsx', sheet_name='nombre archivo')
 
+# Ver el resultado del Excel
+""" for row in df.itertuples():
+    print(row) """
+
+
+# Crear los inserts
 for row in df.itertuples():
-    consulta_sql = "INSERT INTO \"general\".list_param (code, details, active) VALUES ('estrategia', '{\"code\": \"%s\", \"name\": \"%s\"}'::jsonb, true);\n" % (  # noqa: E501
-        row.COD_SOLICITUD, row.Estrategias
+    consulta_sql = "INSERT INTO \"general\".list_param (code, details, active) VALUES ('tipo', '{\"code\": \"%s\", \"name\": \"%s\"}'::jsonb, true);\n" % (  # noqa: E501
+        row.COD_SOLICITUD,
+        row.Estrategias
     )
 
     print(consulta_sql)
